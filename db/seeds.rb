0724ed0054fa208b19ab0e1e0e7666886e4d26b0
sajan <sajanghuman18@gmail.com>
2024-03-13 03:16:04 -0500

    Add pagination with 10 rows per page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,15 @@
 require 'net/http'
 require 'json'
 
-uri = URI('https://api.jikan.moe/v4/anime')
-response = Net::HTTP.get_response(uri)
-anime_list = JSON.parse(response.body)['data']
+csv_file = Rails.root.join('db/anime.csv')
+csv_data = File.read(csv_file)
+anime = CSV.parse(csv_data, headers: true)
 
-anime_list.each do |row|
+anime.each do |row|
     AnimeList.create(
-        eng_name: row["title_english"],
-        jap_name: row["title_japanese"],
-        episode: row["episodes"],
-        status: row["status"],
-        rating: row["rating"],
+        eng_name: row["English name"],
+        jap_name: row["Japanese name"],
+        episode: row["Episodes"],
+        rating: row["Rating"],
     )
 end
